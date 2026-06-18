e2da13a2bef12158257cc6f3aed768211994aec3
shivkantkumar098 <shivkantkr9934@gmail.com>
2026-06-18 10:25:19 +0530

    config Reader added

---
+++ src/main/java/com/javaAT/config/ConfigReader.java
@@ -0,0 +1,27 @@
+package com.javaAT.config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+
+    private static Properties prop = new Properties();
+
+    static {
+
+        try {
+            FileInputStream fis =
+                    new FileInputStream("src/test/resources/config.properties");
+
+            prop.load(fis);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String get(String key) {
+        return prop.getProperty(key);
+    }
+}
