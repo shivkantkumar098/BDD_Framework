a694a9f097c9f021cff3b45f8df7f5253a032201
shivkantkumar098 <shivkantkr9934@gmail.com>
2026-06-19 09:33:19 +0530

    Retry Listner

---
+++ src/main/java/com/javaAT/listners/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package com.javaAT.listners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int MAX_RETRY_COUNT = 3;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < MAX_RETRY_COUNT) {
+            retryCount++;
+            return true; // Retry the test
+        }
+        return false; // Do not retry
+    }
+}
